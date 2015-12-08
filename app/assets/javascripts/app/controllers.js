0d05799dc6d85b08cc752a58e5687ae52965ff01
James Newman <jamesmnewman1@gmail.com>
2015-12-07 21:36:34 -0800

    returning articles

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -1,4 +1,6 @@
-angular.module('myApp.controllers', [])
-  .controller('HomeController', function($scope) {
-
-  });
+angular.module('myApp.controllers', []) 
+  .controller('HomeController',function($scope, ArticleService) { 
+  	ArticleService.getLatestFeed().then(function(data) {
+      $scope.articles = data;
+    })
+});
