72274248927a53ece72dfa1190865a7cf02acf05
James Newman <jamesmnewman1@gmail.com>
2015-12-08 23:05:01 -0800

    cors, just like last time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < ActionController::Base
 
   after_filter  :set_csrf_cookie_for_ng
 
+  before_filter :set_cors_headers
+  before_filter :cors_preflight
+
   private
   def set_csrf_cookie_for_ng
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
@@ -12,5 +15,13 @@ class ApplicationController < ActionController::Base
 
   def verified_request?
     super || form_authenticity_token == request.headers['HTTP_X_XSRF_TOKEN'] 
+  end
+
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin'] = AppConfig.client['origin'] headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS' headers['Access-Control-Allow-Headers'] = '*' headers['Access-Control-Max-Age'] = "3628800"
+  end
+  
+  def cors_preflight
+    head(:ok) if request.method == :options
   end
 end
