1a0b270ddb56c97a7964425b24afa91a61db181f
James Newman <jamesmnewman1@gmail.com>
2015-12-20 22:38:45 -0800

    new users controller...kinda weird structure, but moving along

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,13 +1,13 @@
-class Users::UsersController < Devise::SessionsController
+class Users::UsersController < Devise::SessionsController 
+
+  protect_from_forgery with: :exception, except: [:is_user] 
+
   respond_to :json
 
-  def is_user?
-  	reject_if_not_authorized_request!
-  	render status: 200, json: { success: !User.find_by_name(params[:name]).blank? }
+  def is_user authenticate_user! render status: 200,
+    json: {
+    success: !User.find_by_name(params[:name]).blank?
+    }
   end
 
-  private
-  def reject_if_not_authorized_request! 
-  	warden.authenticate!(scope: resource_name, recall: "#{controller_path}#failure") 
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,11 @@ Rails.application.routes.draw do
       }
 
   devise_scope :user do
-    get '/api/current_user' => 'users/sessions#show_current_user'
-    post '/api/check/is_user' => 'users/users#is_user', as: 'is_user'
+    #get '/api/current_user' => 'users/sessions#show_current_user'
+    #post '/api/check/is_user' => 'users/users#is_user', as: 'is_user'
+
+    post '/check/is_user' => 'users/users#is_user',
+    as: 'is_user'
   end    
 
   get '/dashboard' => 'welcome#dashboard'
