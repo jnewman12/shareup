e0769fc46e5e0f76994b8d75ab816593d4bbfdff
James Newman <jamesmnewman1@gmail.com>
2015-12-24 12:31:33 -0800

    final touch on main

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -7,10 +7,17 @@
  * # MainCtrl
  * Controller of the shareupApp
 */
-App.controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+// App.controller('MainCtrl', function ($scope) {
+//     $scope.awesomeThings = [
+//       'HTML5 Boilerplate',
+//       'AngularJS',
+//       'Karma'
+//     ];
+// });
+
+App.controller('MainCtrl', function($scope, $http, ArticleService) { 
+  $scope.currentUser = {}; 
+  ArticleService.getLatestFeed().then(function(data) {
+    $scope.articles = data;
+  });
 });
