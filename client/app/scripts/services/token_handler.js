b219ad48bb0d5223b5029e704cad7fc18eb1037b
James Newman <jamesmnewman1@gmail.com>
2015-12-23 21:33:55 -0800

    not liking the syntax

--- client/app/scripts/services/token_handler.js
+++ client/app/scripts/services/token_handler.js
@@ -3,7 +3,16 @@
 // were in function args, but jslint complained $http, $q, $location
 
 App.service('tokenHandler', function($rootScope) {
-  var token = null;
+  var token = null, currentUser;
+
+  var tokenWrapper = function(resource, action) {
+    resource['_' + action] = resource[action];
+	resource[action] = function( data, success, error){
+	  return resource['_' + action](
+	    angular.extend({}, data || {}, 
+	      {access_token: tokenHandler.get()}), success,error); 
+	};
+  };
 //currentUser;
   var tokenHandler = {
 	set: function(value) { token = value; },
@@ -14,6 +23,20 @@ App.service('tokenHandler', function($rootScope) {
 		return token;	
 	    }
 	}
+  };
+  getCurrentUser: function() {
+  	var d = $q.defer();
+  	if (currentUser) {
+  		d.resolve(currentUser);
+  	} else {
+  		$http({
+  			url: '/api/current_user',
+  			method: 'POST'
+  		}).then(function(data) {
+  			d.resolve(data.data);
+  		});
+  	}
+  	return d.promise;
   };
   return tokenHandler;
 });
